f171234bbacc4c3609f73020d479a0d018c73a93
Neha Patil <nehadpatil5@gmail.com>
2024-09-24 07:33:21 +0530

    new file pushing to develop

---
+++ CallStaticMethod.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class CallStaticMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Teacher Name : Manish");
+		Student();
+		Student1();
+		Student2();
+		
+	}
+	static void Student()
+	{
+		System.out.println("Student name : Neha");
+	}
+    static void Student1()
+    {
+    	System.out.println("Student1 name : Aniket");
+    	
+    }
+    static void Student2()
+    {
+    	System.out.println("Student2 name : Akshad");
+
+    }
+}
